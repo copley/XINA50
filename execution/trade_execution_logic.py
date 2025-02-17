8ce15b19b270b21bf4df5a6ada6c843231a78782
Max Copley. SWEN Graduate from VUW (Wellington, NZ) <qcopley@gmail.com>
2025-02-18 10:53:43 +1300

    Create trade_execution_logic.py

---
+++ execution/trade_execution_logic.py
@@ -0,0 +1,8 @@
+# execution/trade_execution_logic.py
+
+def dummy_execution():
+    """
+    This is a placeholder to illustrate where
+    you would place actual IB order placement logic.
+    """
+    pass
